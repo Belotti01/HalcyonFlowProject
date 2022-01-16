cec916481002294ae3d329a129e7f9435f7f1fda
Nekai <alessandrobelotti4570@gmail.com>
2022-01-17 00:09:16 +0100

    Add user edit form, accessible from the users list. Now allow the user to reorder and resize grids' columns.

--- Data/Database/Tables/User.cs
+++ Data/Database/Tables/User.cs
@@ -5,6 +5,10 @@
 		public string FullName { get; set; } = string.Empty;
 		[Column("fk_role"), ForeignKey("UserRoles")]
 		public long RoleId { get; set; }
+
+		public Role? GetRole(DB db) {
+			return db.Roles.Find(RoleId);
+        }
 		
 	}
 }
