ecbba32b9b9e4d3eb0ab4e063ff48979f02641c5
Nekai <alessandrobelotti4570@gmail.com>
2022-01-29 16:39:22 +0100

    Add data manipulation interfaces.

---
+++ Interfaces/IDataManipulator.cs
@@ -0,0 +1,14 @@
+﻿namespace HalcyonFlowProject.Interfaces {
+    public interface IDataManipulator {
+        TItem CreateItem<TItem>() where TItem : class, new();
+        void InsertItem<TItem>(TItem item) where TItem : class, new();
+        void UpdateItem<TItem>(TItem item) where TItem : class, new();
+        void DeleteItem<TItem>(TItem item) where TItem : class, new();
+        void UpdateOrInsert<TItem>(TItem item) where TItem : class, new();
+
+        bool TryInsertItem<TItem>(TItem item) where TItem : class, new();
+        bool TryDeleteItem<TItem>(TItem item) where TItem : class, new();
+
+        bool ItemExists<TItem>(TItem item) where TItem : class, new();
+    }
+}
